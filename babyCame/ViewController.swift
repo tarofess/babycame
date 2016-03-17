a5e6ab2c77c0c9cb350d45742e43ac4c60c7cd4a
tarofess <tarofess@email.com>
2016-03-17 22:41:33 +0900

    開発再開コミット！！！！！！

--- babyCame/ViewController.swift
+++ babyCame/ViewController.swift
@@ -15,7 +15,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
     }
     
     override func viewWillDisappear(animated: Bool) {
